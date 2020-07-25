e598660e106b84a9351235903e0f90655b86fcd5
Noéwen <stagiaire03@dawizz.fr>
2020-07-25 17:46:05 +0200

    FAB

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import Head from 'next/head'
 import Gallery from "react-photo-gallery";
 import withWidth, {isWidthUp} from '@material-ui/core/withWidth';
+import React, {useEffect, useState} from 'react'
+import { Brightness4 } from '@material-ui/icons/';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+
 import userInfo from "../util/userinfo.json"
 import tileData from "./api/pic.json"
+
+const useStyles = makeStyles((t) => ({
+    fab: {
+        position: 'absolute',
+        bottom: t.spacing(2),
+        right: t.spacing(2),
+    }
+}));
 
 export default withWidth()(function Home(props) {
     const {width} = props
     const pc = isWidthUp('lg', width)
+    const [theme, setTheme] = useState('black')
+    const classes = useStyles();
+
+    useEffect(() => {
+        setTheme(localStorage.getItem('theme'))
+    }, [])
+
     return (
       <div>
         <Head>
@@ -15,12 +35,29 @@ export default withWidth()(function Home(props) {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
 
+        <Fab
+          className={classes.fab}
+          color="primary"
+          aria-label="add"
+          onClick={() => {
+              if (theme === 'black') {
+                  setTheme('white')
+                  localStorage.setItem('theme', 'white')
+              } else {
+                  setTheme('black')
+                  localStorage.setItem('theme', 'black')
+              }
+        }}
+        >
+          <Brightness4 />
+        </Fab>
+
         <main>
-          <h1 style={{lineHeight: 1.15, fontSize: '4rem', textAlign: 'center'}}>
+          <h1 style={{lineHeight: 1.15, fontSize: '4rem', textAlign: 'center', color: theme === 'black' ? 'white' : 'black'}}>
             {`@${userInfo.username}`}
           </h1>
 
-          <p style={{lineHeight: 1.5, fontSize: '1.5rem', textAlign: 'center'}}>
+          <p style={{lineHeight: 1.5, fontSize: '1.5rem', textAlign: 'center', color: theme === 'black' ? 'white' : 'black'}}>
             {userInfo.description}
           </p>
 
@@ -33,6 +70,7 @@ export default withWidth()(function Home(props) {
           {`
         html,
         body {
+          background-color: ${theme};
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
