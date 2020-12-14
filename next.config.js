bd874a2a4b5198cef2cbb697aa7755ba385badc3
kernoeb <kernoeb@protonmail.com>
2020-12-14 08:57:34 +0100

    update deps, use next optimized images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-const withImages = require('next-images')
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = withImages()
+module.exports = withPlugins([
+    [optimizedImages, {
+        /* config for next-optimized-images */
+    }],
+
+    // your other plugins here
+
+]);
